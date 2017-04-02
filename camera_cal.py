6522ac5be42c76b77c5bac49dc96123655934e65
joezq <zhouqi.coding@gmail.com>
2017-04-02 10:32:37 +0800

    WIP sliding window

--- camera_cal.py
+++ camera_cal.py
@@ -238,11 +238,21 @@ def color_threshold(image, thresh = (0, 255)):
     return s_binary
 
 
+def combine_gradient(img):
+    gradx = sobelX_transform(img)
+    # grady = sobelY_transform(warped)
+    grad_mag = mag_gradient(img, mag_thresh=(33, 255))
+    #grad_dir = dir_gradient(img, thresh=(0.6, 2))
+    color = color_threshold(img, thresh=(65, 255))
+
+    combined = np.zeros_like(gradx)
+    combined[((gradx == 1) | grad_mag == 1) & (color == 1)] = 1
+    return combined
+
 def test_threshold():
     #img = cv2.imread('test_images/straight_lines2.jpg')
     img = cv2.imread('test_images/test2.jpg')
     #test4 is a stress test
-    #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     # undistort the image
     with open('imgpoints.pickle', 'rb') as f:
@@ -253,14 +263,7 @@ def test_threshold():
 
     warped = perspective_warp(undist)
 
-    gradx= sobelX_transform(warped)
-    #grady = sobelY_transform(warped)
-    grad_mag = mag_gradient(warped, mag_thresh=(33,255))
-    grad_dir = dir_gradient(warped,thresh=(0.6, 2))
-    color = color_threshold(warped, thresh=(65,255))
-
-    combined = np.zeros_like(gradx)
-    combined[((gradx == 1) | grad_mag ==1) & (color ==1) ] = 1
+    combined = combine_gradient(warped)
 
     plt.imshow(combined, cmap='gray')
     plt.show()
@@ -271,3 +274,121 @@ def test_threshold():
 ################
 #6. Find the lane
 ################
+def find_lane(img):
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(img[int(img.shape[0] / 2):, :], axis=0)
+
+    #an output image to draw the results
+    out_img = np.dstack((img, img, img)) * 255 #3 channels
+
+    # Find the peak of the left and right halves of the histogram
+    midpoint = np.int(histogram.shape[0] / 2) #midpoint of the graph
+    leftx_base = np.argmax(histogram[:midpoint]) #left peak
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint # right peak
+
+    #9 sliding window
+    nwindows = 2
+
+    window_height = np.int(img.shape[0] / nwindows) #y axis for each window
+
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = img.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+
+    # Current positions to be updated for each window
+    #current position is the peak of the histogram
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+
+    # Set the width of the windows +/- margin
+    margin = 57
+    # Set minimum number of pixels found to recenter window
+    minpix = 45
+
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+
+        #moving from top to bottom
+        win_y_low = img.shape[0] - (window + 1) * window_height
+        win_y_high = img.shape[0] - window * window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (0, 255, 0), 2)
+        cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 2)
+
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (
+        nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (
+        nonzerox < win_xright_high)).nonzero()[0]
+
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+        # Concatenate the arrays of indices
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+
+        # Extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds]
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+
+        # Fit a second order polynomial to each
+        left_fit = np.polyfit(lefty, leftx, 2) # fit polynomial across all visible points
+        right_fit = np.polyfit(righty, rightx, 2)
+
+
+
+    # plot the chart
+    ploty = np.linspace(0, img.shape[0] - 1, img.shape[0])
+    left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+    right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+    plt.imshow(out_img)
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+    plt.xlim(0, 1280)
+    plt.ylim(720, 0)
+    plt.show()
+
+
+def test_find_lane():
+    img = cv2.imread('test_images/straight_lines1.jpg')
+    #img = cv2.imread('test_images/test2.jpg')
+    # test4 is a stress test
+    # undistort the image
+    with open('imgpoints.pickle', 'rb') as f:
+        imgpoints = pickle.load(f)
+    with open('objpoints.pickle', 'rb') as f:
+        objpoints = pickle.load(f)
+    undist = undistort(img, objpoints, imgpoints)
+
+    warped = perspective_warp(undist)
+
+    combined = combine_gradient(warped)
+
+    find_lane(combined)
+
+test_find_lane()
